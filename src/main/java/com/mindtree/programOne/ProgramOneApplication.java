c54a6dd6a4c9dd05133e5dc8e82b0b7a32b92d74
vibhavanya <vibha.vanya@gmail.com>
2019-12-10 05:21:20 +0000

    First Commit

---
+++ src/main/java/com/mindtree/programOne/ProgramOneApplication.java
@@ -0,0 +1,13 @@
+package com.mindtree.programOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramOneApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mindtree/programOne/controller/Login.java
@@ -0,0 +1,21 @@
+package com.mindtree.programOne.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Login {
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+
+	@PostMapping("/hello")
+	public String sayHello(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+}
